82bb4f9efff7c02162193e4bba8cc2280b9befcd
27achang <27achang@shcp.edu>
2024-09-09 15:54:43 -0700

    Create student_details.js

---
+++ assets/student_details.js
@@ -0,0 +1,12 @@
+class Student{
+    constructor(img_src, first_name, last_name, profile_path){
+        this.img_src = img_src;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.profile_path = profile_path;
+    }
+}
+
+const students = {
+    
+};
